27d9cc6e881650c635099740952841055e928292
李政秋 <lizhengqiu@riseiakis-MacBook-puro.local>
2020-07-10 13:31:08 -0400

    code challenge

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,43 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.undo_stack = []
+        self.redo_stack = []
 
     def add(self, num: int):
-        pass
+        self.undo_stack.append(num)
+        self.value += num
+        # self.redo_stack = []
 
     def subtract(self, num: int):
-        pass
+        self.undo_stack.append(0-num)
+        self.value -= num
+        # self.redo_stack = []
 
     def undo(self):
-        pass
+        if len(self.undo_stack) != 0:
+            undo_value = self.undo_stack.pop()
+            self.redo_stack.append(undo_value)
+            self.value -= undo_value
 
     def redo(self):
-        pass
+        if len(self.redo_stack) != 0:
+            redo_value = self.redo_stack.pop()
+            self.undo_stack.append(redo_value)
+            self.value += redo_value
 
     def bulk_undo(self, steps: int):
-        pass
+        max_undo = len(self.undo_stack)
+        index = min(max_undo,steps)
+        for i in range(index):
+            undo_value = self.undo_stack.pop()
+            self.redo_stack.append(undo_value)
+            self.value -= undo_value
 
     def bulk_redo(self, steps: int):
-        pass
+        max_redo = len(self.redo_stack)
+        index = min(max_redo,steps)
+        for i in range(index):
+            redo_value = self.redo_stack.pop()
+            self.undo_stack.append(redo_value)
+            self.value += redo_value
